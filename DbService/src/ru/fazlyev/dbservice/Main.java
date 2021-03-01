95056f6fd98e030b0ebc81fba58500bbb200b277
Azat Fazlyev <fazliev14@gmail.com>
2021-03-01 21:28:03 +0300

    Initial commit

---
+++ DbService/src/ru/fazlyev/dbservice/Main.java
@@ -0,0 +1,7 @@
+package ru.fazlyev.dbservice;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ PersonService/src/ru/fazlyev/personservice/Main.java
@@ -0,0 +1,7 @@
+package ru.fazlyev.personservice;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
